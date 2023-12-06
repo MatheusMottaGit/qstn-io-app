3ecdf4cdc8ec46442222a91d0078cc58bc5e58fb
Matheus Motta <matheusdomingues423@gmail.com>
2023-12-05 21:35:24 -0300

    feat: improving sidebar

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -9,6 +9,7 @@ import {
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import React from "react";
+import Profile from "./profile";
 
 const links = [
   {
@@ -19,18 +20,12 @@ const links = [
   },
   {
     id: "2",
-    icon: <Waypoints className="w-4 h-4" />,
-    page: "Serviços",
-    path: "/frequencias",
+    icon: <BookCopy className="w-4 h-4" />,
+    page: "Questionários",
+    path: "/questionarios",
   },
   {
     id: "3",
-    icon: <BookCopy className="w-4 h-4" />,
-    page: "Questionários",
-    path: "/avaliacoes",
-  },
-  {
-    id: "4",
     page: "Configurações",
     icon: <Settings className="w-4 h-4" />,
     path: "/conteudo",
@@ -41,33 +36,36 @@ const Sidebar = () => {
   const currentPath = usePathname();
 
   return (
-    <aside className="w-48 h-screen px-3 py-4 bg-zinc-50 flex flex-col gap-10 shadow">
-      <div className="text-strong flex items-center gap-1 font-bold">
-        <Blocks />
-        qstn.ui
+    <aside className="w-48 h-screen px-3 py-4 bg-zinc-50 flex flex-col justify-between gap-10 shadow">
+      <div className="space-y-8">
+        <div className="text-strong flex items-center gap-1 font-bold">
+          <Blocks />
+          qstn.ui
+        </div>
+
+        <ul className="flex flex-col gap-5 text-sm text-muted-foreground/50">
+          {links.map((link) => {
+            return (
+              <li
+                data-path={
+                  currentPath === link.path ||
+                  currentPath.startsWith(`${link.path}/`)
+                }
+                key={link.id}
+                className="rounded-md p-2 hover:bg-main cursor-pointer hover:text-white transition-colors flex items-center justify-between data-[path=true]:bg-main data-[path=true]:text-white"
+              >
+                <div className="flex items-center gap-2">
+                  {link.icon}
+
+                  <a href={link.path}>{link.page}</a>
+                </div>
+
+                <ChevronRight className="w-4 h-4" />
+              </li>
+            );
+          })}
+        </ul>
       </div>
-
-      <ul className="flex flex-col gap-5 text-sm text-muted-foreground/50">
-        {links.map((link) => {
-          return (
-            <li
-              data-path={link.path === currentPath}
-              key={link.id}
-              className="rounded-md p-2 hover:bg-main hover:text-white transition-colors flex items-center justify-between data-[path=true]:bg-main data-[path=true]:text-white"
-            >
-              <div className="flex items-center gap-2">
-                {link.icon}
-
-                <a href={link.path}>{link.page}</a>
-              </div>
-
-              <ChevronRight className="w-4 h-4" />
-            </li>
-          );
-        })}
-      </ul>
-
-      <div></div>
     </aside>
   );
 };
