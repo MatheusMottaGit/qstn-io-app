87472b79e77344c1c6828a6ade78a8ced433062b
Matheus Motta <matheusdomingues423@gmail.com>
2023-12-05 21:37:16 -0300

    feat: build share quiz form

---
+++ web/src/components/quiz/share-quiz-form.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Copy, Share2 } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+
+interface ShareProps {
+  quizId: string;
+}
+
+const ShareQuizForm = (props: ShareProps) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-main font-semibold w-fit gap-1">
+          <Share2 className="w-4 h-4" />
+          Compartilhar
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Compartilhe com as pessoas!</DialogTitle>
+          <DialogDescription>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </DialogDescription>
+        </DialogHeader>
+
+        {/*quiz id copy clipboard form*/}
+        <div className="flex gap-2">
+          <Input value={props.quizId} className="text-slate-400 font-medium" />
+
+          <Button className="gap-1">
+            Copiar
+            <Copy className="w-4 h-4" />
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ShareQuizForm;
